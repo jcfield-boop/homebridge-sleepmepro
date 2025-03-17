1a250339f3fa49e0514ee0396c9e264d2b81b96d
James Field <jcfield+git@gmail.com>
2025-03-17 12:43:17 -0700

    more tests

---
+++ test-module.js
@@ -0,0 +1,13 @@
+try {
+    require('@typescript-eslint/parser');
+    console.log('@typescript-eslint/parser found');
+  } catch (error) {
+    console.error('@typescript-eslint/parser not found:', error.message);
+  }
+  
+  try {
+    require('@typescript-eslint/eslint-plugin');
+    console.log('@typescript-eslint/eslint-plugin found');
+  } catch (error) {
+    console.error('@typescript-eslint/eslint-plugin not found:', error.message);
+  }
