3649d8dd0293ca3d150317e9c918617c5a433a11
James Field <jcfield+git@gmail.com>
2025-03-17 12:25:52 -0700

    troubleshooting

--- eslint.config.ts.js
+++ eslint.config.ts.js
@@ -1,50 +1,13 @@
-import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
-  {
-    ignores: ['dist/**'],
-  },
   {
     files: ['src/**/*.ts'],
     languageOptions: {
       parser: tseslint.parser,
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname, // Add this line
-      },
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        node: true,
-      },
     },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
     },
-    rules: {
-      quotes: ['error', 'single'],
-      indent: ['error', 2, { SwitchCase: 0 }],
-      'linebreak-style': ['error', 'unix'],
-      semi: ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'dot-notation': 'error',
-      eqeqeq: ['error', 'smart'],
-      curly: ['error', 'all'],
-      'brace-style': ['error'],
-      'prefer-arrow-callback': 'warn',
-      'max-len': ['warn', 160],
-      'object-curly-spacing': ['error', 'always'],
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': ['error', { classes: false, enums: false }],
-      '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'none' }],
-    },
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  }
 );
