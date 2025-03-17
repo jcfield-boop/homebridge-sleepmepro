409e1976ede54be2e68c36b59b14298b80a24e46
James Field <jcfield+git@gmail.com>
2025-03-17 11:31:45 -0700

    a few lint config issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,32 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
   {
     ignores: ['dist/**'],
   },
   {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        node: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
     rules: {
       'quotes': ['error', 'single'],
       'indent': ['error', 2, { 'SwitchCase': 0 }],
@@ -22,12 +43,6 @@ export default tseslint.config(
       'no-use-before-define': 'off',
       '@typescript-eslint/no-use-before-define': ['error', { 'classes': false, 'enums': false }],
       '@typescript-eslint/no-unused-vars': ['error', { 'caughtErrors': 'none' }],
-    },
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
     },
   },
   eslint.configs.recommended,
